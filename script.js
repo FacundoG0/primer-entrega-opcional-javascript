ef9835114b1a9d6108cade3d1bf92bfaac543627
Facundo Gordillo <gordillofacundo44@gmail.com>
2022-07-21 07:15:21 -0300

    bucle while do

--- script.js
+++ script.js
@@ -122,10 +122,10 @@ let ingreso = parseInt(prompt("1) Arte 2) Historia 3) Deportes 4) Ciencias"));
 //     }
 // }
 
-switch (ingreso){
-    case 1: ingreso === 1;
-    alert("¿Cómo es el nombre del artista que creó la pintura llamada 'El grito'?");
-    let respuesta_arte = parseInt(prompt("1) Leonardo Da Vinci 2) Pablo Pîcasso 3) Edvard Munch 4) DiCaprio"));
+do{
+    if (ingreso === 1){
+        alert("¿Cómo es el nombre del artista que creó la pintura llamada 'El grito'?");
+        let respuesta_arte = parseInt(prompt("1) Leonardo Da Vinci 2) Pablo Pîcasso 3) Edvard Munch 4) DiCaprio"));
 
     // *******************************ARTE**********************************
         for (respuesta_arte; respuesta_arte != 3;){
@@ -154,39 +154,44 @@ switch (ingreso){
         }
 
         verificacion_jugar_nuevamente();
-        break;
+    }
 
     // *******************************HISTORIA**********************************
-    case 2: ingreso === 2;
-    alert("¿Quién fue el primer hombre en pisar la luna?");
-    let respuesta_historia = parseInt(prompt("1) Federich Tereshckova 2) Neil Amstrong 3) Edvard Munch 4) Buzz Lightyear"));
+    else if (ingreso === 2){
+        alert("¿Quién fue el primer hombre en pisar la luna?");
+        let respuesta_historia = parseInt(prompt("1) Federich Tereshckova 2) Neil Amstrong 3) Edvard Munch 4) Buzz Lightyear"));
 
         for (respuesta_historia; respuesta_historia != 2;){
             alert("¡Ups! Respuesta incorrecta, intente nuevamente");
             respuesta_historia = parseInt(prompt("1) Federich Tereshckova 2) Neil Amstrong 3) Edvard Munch 4) Buzz Lightyear"));
         }
 
-    alert("¡Muy Bien! ¡Sigue así!");
-    alert("¿Desea jugar nuevamente?");
-    jugar_nuevamente = parseInt(prompt("1) Si 2) No"));
-    verificacion_jugar_nuevamente();
-    break;
+        alert("¡Muy Bien! ¡Sigue así!");
+        alert("¿Desea jugar nuevamente?");
+        jugar_nuevamente = parseInt(prompt("1) Si 2) No"));
+        verificacion_jugar_nuevamente();
+    }
 
-    case 3: ingreso === 3;
-    alert("En el boxeo: ¿Qué significa 'TKO'?");
-    let respuesta_deportes = parseInt(prompt("1) Technical knockout 2) Total Knockout 3) Terminal Knockout 4) Opponent kicked"));
+    else if (ingreso === 3){
+        alert("En el boxeo: ¿Qué significa 'TKO'?");
+        let respuesta_deportes = parseInt(prompt("1) Technical knockout 2) Total Knockout 3) Terminal Knockout 4) Opponent kicked"));
         
     for (respuesta_deportes; respuesta_deportes != 1;){
         alert("¡Ups! Respuesta incorrecta, intente nuevamente");
         respuesta_deportes = parseInt(prompt("1) Technical knockout 2) Total Knockout 3) Terminal Knockout 4) Opponent kicked"));
     }
 
-    alert("¡Muy Bien! ¡Sigue así!");
-    alert("¿Desea jugar nuevamente?");
-    jugar_nuevamente = parseInt(prompt("1) Si 2) No"));
-    verificacion_jugar_nuevamente();
-    break;
+        alert("¡Muy Bien! ¡Sigue así!");
+        alert("¿Desea jugar nuevamente?");
+        jugar_nuevamente = parseInt(prompt("1) Si 2) No"));
+        verificacion_jugar_nuevamente();
+    }
     
-    default:
-        alert("¡Ingrese una opción correcta!");
-};
+    else{
+        for (ingreso; ingreso > 4 || ingreso < 1;){
+            alert("Ingrese una opción válida")
+            ingreso = parseInt(prompt("1) Arte 2) Historia 3) Deportes 4) Ciencias"));
+        }
+    };
+}
+while (ingreso <= 4 && ingreso >= 1 && ingreso != null);
